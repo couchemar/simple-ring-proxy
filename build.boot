1c60b41dcdba08c2980fe3e9e03980a4fc63d5f6
Andrey Pavlov <couchemar@yandex.ru>
2017-07-07 08:35:42 +0300

    Simple proxy

---
+++ build.boot
@@ -0,0 +1,21 @@
+(set-env!
+ :source-paths #{"src"}
+
+ :dependencies '[
+                 [org.clojure/clojure "1.9.0-alpha17"]
+                 [ring/ring-core "1.6.1"]
+                 [clj-http "3.6.1"] 
+                 [adzerk/bootlaces "0.1.13" :scope "test"]
+                 ])
+
+(require '[adzerk.bootlaces :refer :all])
+
+(def +version+ "0.0.1")
+
+(task-options!
+ pom {:project 'couchemar.simple-ring-proxy
+      :version +version+
+      :description "HTTP proxy ring middleware"
+      :scm {:url "https://github.com/couchemar/simple-ring-proxy"}})
+
+(bootlaces! +version+)
---
+++ src/couchemar/simple_ring_proxy.clj
@@ -0,0 +1,45 @@
+(ns couchemar.simple-ring-proxy
+  (:require [clojure.string :refer [replace]]
+            [clj-http.client]
+            [ring.middleware.cookies :refer [wrap-cookies]]))
+
+(defn- prepare-request
+  [uri request]
+  {:method (:request-method request)
+   :url (str uri "?" (:query-string request))
+   :headers (dissoc (:headers request) "host" "content-length")
+   :body (:body request)
+   :follow-redirects true
+   :throw-exceptions false
+   :as :stream})
+
+(def ^:private valid-cookie-attrs
+  [:domain :max-age :path :secure
+   :expires :http-only :same-site
+   :value])
+
+(defn- prepare-cookie
+  [cookie]
+  (let [valid-attrs (-> cookie
+                        (get-in [1])
+                        (select-keys valid-cookie-attrs)
+                        (update-in [:expires] str))]
+    (assoc-in cookie [1] valid-attrs)))
+
+(defn- process-response
+  [response]
+  (let [cookies (:cookies response)]
+    (assoc response :cookies (into {} (map prepare-cookie cookies)))))
+
+(defn wrap-proxy
+  [handler ^String req-path-re proxy-replace-re & [http-opts]]
+  (wrap-cookies
+   (fn [request]
+
+     (if (re-matches req-path-re (:uri request))
+       (-> (replace (:uri request) req-path-re proxy-replace-re)
+           (prepare-request request)
+           (merge http-opts)
+           clj-http.client/request
+           process-response)
+       (handler request)))))
